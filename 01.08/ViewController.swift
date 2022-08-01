111dc22e8719a1c20245e423916960fba973ea6b
Rashad Humbatov <rashadhumbatov@gmail.com>
2022-08-01 09:16:22 +0400

    Initial Commit

---
+++ 01.08/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  01.08
+//
+//  Created by Rashad Humbatov on 01.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
